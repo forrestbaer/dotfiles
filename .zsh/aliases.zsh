3548b39e2777a3a395ef4caff347821b4c450ff9
Gerritt Baer <gfbaer@gmail.com>
2012-03-20 11:45:00 -0700

    added some git aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,3 +4,14 @@ alias la='ls -alF' # include dot files in listing; add / to end of directories
 alias mv='mv -i' # prompt before moving a file that would overwrite an existing file
 alias grep='grep --color --line-number'
 alias c='clear'
+
+# Git
+
+alias st='git status'
+alias gstat='git status'
+alias gc='git commit'
+alias gd='git diff'
+alias gp='git push'
+alias getch='git fetch'
+alias hist='git hist'
+alias ga='git add'
