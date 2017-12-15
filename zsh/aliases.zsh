5590c7dc1597c993dc8d0b03c1ca4c05d65113f4
Forrest Baer <gfbaer@gmail.com>
2017-12-15 00:35:46 -0700

    Updating git hist colors, and some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 alias ls='ls -GhF'
-alias ll='ls -GhlF' # long format listing; add / to end of directories
+alias ll='ls -GhlFa' # long format listing; add / to end of directories
 alias la='ls -GalaF' # include dot files in listing; add / to end of directories
 alias mv='mv -i' # prompt before moving a file that would overwrite an existing file
 alias grep='grep --color --line-number'
@@ -7,13 +7,14 @@ alias c='clear'
 
 # Git
 
-alias st='git status'
+alias gs='git status'
 alias gstatus='git status'
 alias gc='git commit'
 alias gd='git diff'
 alias gp='git push'
 alias getch='git fetch'
 alias hist='git hist'
+alias gh="git hist"
 alias ga='git add'
 
 # tmux
