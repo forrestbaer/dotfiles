2b4f5bd52c80fd6a8910d7cd2814b1498e28a083
Forrest BafFerF <GBaer@vailresorts.com>
2018-09-10 16:00:22 -0600

    adding ctrl+o to hotkeys to ignore

---
+++ vscode/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ]
+    }
+};
