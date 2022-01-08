7fc501fb92ec86c5cdbf53dd98503e5aac449ef0
Forrest Baer <forrest@forrestbaer.com>
2022-01-08 08:33:45 -0600

    packer bootstrap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,11 +8,6 @@ local fn, opt, api, cmd, g = vim.fn, vim.opt, vim.api, vim.cmd, vim.g
 -- Plugins
 --
 require('plugins')
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
 
 --
 -- helper functions
