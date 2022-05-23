1936ab660c4129cb9bad3546f220a95de874b438
Forrest Baer <forrest@forrestbaer.com>
2022-05-22 23:08:58 -0500

    updated quite a bit

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,21 +6,57 @@ local fn, opt, api, cmd, g = vim.fn, vim.opt, vim.api, vim.cmd, vim.g
 --
 -- plugins
 --
+
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+opt.termguicolors = true
+
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  Packer_Bootstrap = true
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
+
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- packer.init {
+--   display = {
+--     open_fn = function()
+--       return require("packer.util").float { border = "rounded" }
+--     end,
+--   },
+-- }
 
 require('packer').startup({function(use)
   use 'wbthomason/packer.nvim'
-  use 'neovim/nvim-lspconfig'
+
   use 'forrestbaer/minimal_dark'
+
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+  use "windwp/nvim-autopairs"
   use 'nvim-lua/plenary.nvim'
-  use 'rmagatti/goto-preview'
+  use {
+    'rmagatti/goto-preview',
+    config = function()
+      require('goto-preview').setup {}
+    end
+  }
+
   use 'nvim-telescope/telescope.nvim'
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
   use 'nvim-telescope/telescope-file-browser.nvim'
+
   use 'kyazdani42/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
   use 'tpope/vim-surround'
@@ -28,33 +64,33 @@ require('packer').startup({function(use)
   use 'tpope/vim-commentary'
   use 'airblade/vim-gitgutter'
   use 'akinsho/toggleterm.nvim'
-  use {'ms-jpq/coq_nvim', branch = 'coq'}
-  use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
-	use {'tidalcycles/vim-tidal', ft = 'tidal'}
-  use {'fatih/vim-go', ft = 'go'}
+
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
+  use "saadparwaiz1/cmp_luasnip"
+  use "hrsh7th/cmp-nvim-lsp"
 
   use {
-    'vimwiki/vimwiki',
-    config = function()
-      vim.g.vimwiki_list = {{
-        path = '~/store/wiki',
-        syntax = 'markdown',
-        ext = '.md'
-      }}
-    end
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "JoosepAlviste/nvim-ts-context-commentstring"
+
+  use "L3MON4D3/LuaSnip"
+  use "rafamadriz/friendly-snippets"
+
+  use {
+    'norcalli/nvim-colorizer.lua',
+    ft = { 'css', 'javascript', 'vim', 'html', 'typescript' },
+    config = [[require('colorizer').setup {'css', 'javascript', 'vim', 'html', 'typescript'}]],
   }
 
-  if Packer_Bootstrap then
+  if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
-end,
-config = {
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'single' })
-    end
-  }
-}})
+end})
 
 
 --
@@ -95,10 +131,27 @@ end
 --
 -- lsp
 --
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
+
+require("nvim-lsp-installer").setup({
+    automatic_installation = true,
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
+
 lspconfig.sumneko_lua.setup{
   settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
 }
+
+local servers = { 'html', 'tsserver', 'gopls'}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {}
+end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -106,43 +159,17 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     virtual_text = true,
     signs = true,
     update_in_insert = false,
+    severity_sort = true,
   }
 )
-
-local coq = require "coq"
-
-local servers = { 'html', 'tsserver', 'gopls'}
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {coq.lsp_ensure_capabilities{}}
-end
-
-vim.cmd('COQnow -s')
-
-g.coq_settings = {
-  clients = {
-    buffers = {
-      enabled = false,
-      same_filetype = true
-    }
-  }
-}
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics, {
-		virtual_text = false,
-		underline = true,
-		signs = true,
-	}
-)
-
-require('goto-preview').setup{}
 
 --
 -- telescope stuff
 --
---
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
+
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 local telescope_custom_actions = {}
 
 local multiopen = function(prompt_bufnr, open_cmd)
@@ -178,8 +205,7 @@ function telescope_custom_actions.multi_selection_open_tab(prompt_bufnr)
     multiopen(prompt_bufnr, "tabe")
 end
 
-require('telescope').load_extension('fzf')
-require('telescope').setup{
+telescope.setup{
   defaults = {
     prompt_prefix = ' $ ',
     initial_mode = "insert",
@@ -209,7 +235,138 @@ require('telescope').setup{
     },
   },
 }
-require("telescope").load_extension "file_browser"
+
+telescope.load_extension('fzf')
+telescope.load_extension "file_browser"
+
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  mapping = {
+    ["<Up>"] = cmp.mapping.select_prev_item(),
+		["<Down>"] = cmp.mapping.select_next_item(),
+    ["<C-i>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-q>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif check_backspace() then
+        fallback()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  window = {
+    bordered = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
+  },
+}
 
 
 --
@@ -218,7 +375,7 @@ require("telescope").load_extension "file_browser"
 require('nvim-web-devicons').setup{ default = true }
 
 
-require'lualine'.setup{
+require('lualine').setup{
   options = {
     icons_enabled = true,
     fmt = string.lower,
@@ -301,9 +458,10 @@ local Terminal  = require('toggleterm.terminal').Terminal
 local gitui = Terminal:new({
   cmd = "gitui",
   dir = ".",
+  hidden = true,
   direction = "float",
   close_on_exit = true,
-  float_opts = { border = "single", },
+  float_opts = { border = "single" },
   on_open = function(term)
     vim.cmd("startinsert!")
     vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
@@ -322,9 +480,10 @@ cmd 'colorscheme minimal_dark'
 g.gitgutter_sign_added = '+'
 g.gitgutter_sign_modified = '~'
 g.gitgutter_sign_removed = '-'
-g.gitgutter_sign_removed_first_line = '^^'
+g.gitgutter_sign_removed_first_line = '^'
 g.gitgutter_sign_removed_above_and_below = '{'
-g.gitgutter_sign_modified_removed = '-'
+g.gitgutter_sign_modified_removed = '~-'
+g.gitgutter_map_keys = 0
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
@@ -342,6 +501,7 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.showmatch = true
+opt.signcolumn = "yes"
 opt.number = true
 opt.numberwidth = 5
 opt.hidden = true
@@ -375,12 +535,6 @@ g.maplocalleader = ','
 g.gitgutter_terminal_reports_focus = 0
 g.terminal_color_3 = '#ac882f'
 g.tidal_target = "terminal"
-g.go_gopls_enabled = 1
-g.go_auto_type_info = 1
-g.go_info_mode = 'gopls' -- or guru
-g.go_doc_balloon = 1
-g.go_doc_popup_window = 1
-g.go_def_reuse_buffer = 1
 
 
 --
@@ -408,7 +562,7 @@ map('n', '<C-z>', '<C-w>W')
 map('i', '<C-z>', '<C-w>W')
 map('', '<leader>t', '<cmd>ToggleTerm<CR>')
 map('t', '<leader>t', '<cmd>ToggleTerm<CR>')
-map("n", "<leader>g", "<cmd>lua GituiToggle()<CR>")
+map("n", "<leader>G", "<cmd>lua GituiToggle()<CR>")
 
 -- telescope
 map('', '<leader>ff', '<cmd>Telescope find_files<CR>')
@@ -442,7 +596,7 @@ local autocmds = {
       { 'FileType', 'tidal', 'setlocal commentstring=--%s' },
     },
     packer = {
-      { 'BufWritePost', 'init.lua', 'source ~/.config/nvim/init.lua | PackerCompile' },
+      { 'BufWritePost', '~/.config/nvim/init.lua', 'source ~/.config/nvim/init.lua | PackerCompile' },
     },
 }
 nvim_create_augroups(autocmds)
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -17,7 +17,7 @@ fi
 
 export CLICOLOR=1
 export LSCOLORS=dxfxcxdxGxegedabagacad
-export PATH=/usr/local/bin:/usr/local/sbin:~/avr32-tools/bin:~/Library/Python/3.9/lib/python/site-packages:~/Library/Python/3.9/bin:~/.local/bin:~/.cargo/bin:~/bin:~/go/bin:/usr/bin:/bin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:~/avr32-tools/bin:~/Library/Python/3.9/lib/python/site-packages:~/Library/Python/3.9/bin:~/.local/bin:~/bin:/usr/bin:/bin:$PATH
 
 export NVM_DIR="$HOME/.nvm"
 
